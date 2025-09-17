c49b44ac8930f2855d549a453d8d204571c1e356
Axel Porras <axelporrasg@gmail.com>
2025-09-17 08:19:25 -0400

    estructura vistas-componentesDeClase

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout title="Dashboard administrador">
+    <h1>Dashboard ADMIN</h1>
+</x-app-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-</head>
-<body>
+<x-app-layout title="Pagina Principal">
     <div class="max-w-4xl mx-auto p-4">
         <h1>Bienvenidos a la pagina de la estudiantina!!</h1>
 
@@ -17,5 +8,4 @@
 
         <p class="p-4">Hola mundo</p>
     </div>
-</body>
-</html>
+</x-app-layout>
