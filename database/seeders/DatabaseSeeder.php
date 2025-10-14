8e03dafe3469959eb6c7ad1132dbca194fde1fb3
Jose Briceño <gregorio1502@hotmail.com>
2025-10-14 13:46:00 -0400

    feat(seeders): agregar usuario administrador en DatabaseSeeder
    fix(header): corregir enlace de logo en header-landing
    
    style(dashboard): ajustar estilos y eliminar enlace de cerrar sesión en dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => "Admin_Prueba",
+            'email' => "Admin_Prueba@unet.edu.ve",
+            'email_verified_at' => now(),
+            'password' => Hash::make('password_unet'),
+            'es_escritor' => true
+        ]);
 
         $this->call([Factory_Seeder::class]);
     }
