840940fc494c470206ba17f328eb6b2e7a0a36ce
Axel Porras <axelporrasg@gmail.com>
2025-10-20 17:12:09 -0400

    probar el proyecto en movil

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        // Usa '0.0.0.0' para escuchar en todas las interfaces de red
+        host: '0.0.0.0', 
+        // ¡IMPORTANTE! Fuerza al cliente HMR a usar la IP de tu PC
+        hmr: {
+            host: '192.168.0.102', 
+        }
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/partituras.js'],
